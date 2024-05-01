5bf0a0512f076d745a4e4e2f4a81d5c9a4e5222b
hemahemapathi <you@example.com>
2024-05-01 11:46:47 +0530

    adding the script.js file

---
+++ script.js
@@ -0,0 +1,22 @@
+var resumeData = {
+    name: "Hemapathi",
+    age: 22,
+    email: "hemapathi0711@gmail.com",
+    phone: "0123456789",
+    
+  };
+  //1. for loop
+  for (var key in resumeData) {
+    if (resumeData.hasOwnProperty(key)) {
+      console.log(key + ": " + JSON.stringify(resumeData[key]));
+    }
+  }
+  //2.for..in loop
+  for (var section in resumeData) {
+    console.log(section + ": " + JSON.stringify(resumeData[section]));
+    if (typeof resumeData[section] === 'object') {
+      for (var subKey in resumeData[section]) {
+        console.log("  " + subKey + ": " + JSON.stringify(resumeData[section][subKey]));
+      }
+    }
+  }
